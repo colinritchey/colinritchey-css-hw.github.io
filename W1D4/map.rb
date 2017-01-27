86d4e0195ed90d51e403c463303925061d4dbf94
Colin <colin@samsamskies.local>
2017-01-26 21:56:39 -0800

    Create assign(key, value) method and some test cases

--- W1D4/map.rb
+++ W1D4/map.rb
@@ -4,4 +4,14 @@ class Map
   def initialize
     @set = []
   end
+
+  def assign(key, value)
+    @set << [key, value]
+  end
 end
+
+map_set = Map.new
+map_set.assign(1, "hello")
+map_set.assign(2, "world")
+
+p map_set.set
