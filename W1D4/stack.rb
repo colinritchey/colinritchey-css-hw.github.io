7255789faec2cabaf1f8fb2d9336c76e1a3d211a
Colin <colin@samsamskies.local>
2017-01-26 21:32:27 -0800

    Update Stack class to run all functions

--- W1D4/stack.rb
+++ W1D4/stack.rb
@@ -1,17 +1,29 @@
 class Stack
+  attr_accessor :set
+
   def initialize
     # create ivar to store stack here!
+    @set = []
   end
 
   def add(el)
     # adds an element to the stack
+    @set.push(el)
   end
 
   def remove
     # removes one element from the stack
+    @set.pop
   end
 
   def show
     # return a copy of the stack
+    copy = Stack.new
+
+    self.set.each do |item|
+      copy.add(item)
+    end
+
+    copy
   end
 end
