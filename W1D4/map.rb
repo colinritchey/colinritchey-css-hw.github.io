c23d24b33edbbf26a9cd4e6389fc8492a19e2ee7
Colin <colin@samsamskies.local>
2017-01-26 22:02:19 -0800

    Creates lookup(key) that returns a value, test cases at the bottom

--- W1D4/map.rb
+++ W1D4/map.rb
@@ -8,6 +8,16 @@ class Map
   def assign(key, value)
     @set << [key, value]
   end
+
+  def lookup(key)
+    val = nil
+
+    @set.each do |item|
+      val = item if item.first == key
+    end
+
+    val
+  end
 end
 
 map_set = Map.new
@@ -15,3 +25,5 @@ map_set.assign(1, "hello")
 map_set.assign(2, "world")
 
 p map_set.set
+
+p map_set.lookup(3)
