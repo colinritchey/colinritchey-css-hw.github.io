88bd4c72cd6fee17063707c08c723a212e7a123d
Colin Ritchey <colinritchey256@gmail.com>
2017-01-26 21:44:10 -0800

    Got enqueue, dequeue, show to work

--- W1D4/queue.rb
+++ W1D4/queue.rb
@@ -6,15 +6,21 @@ class Queue
   end
 
   def enqueue(el)
-
+    @set.push(el)
   end
 
   def dequeue
-
+    @set.shift
   end
 
   def show
+    copy = Queue.new
 
+    self.set.each do |item|
+      copy.enqueue(item)
+    end
+
+    copy
   end
 
 end
